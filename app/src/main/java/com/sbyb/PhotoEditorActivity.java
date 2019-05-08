085b4be4454e02b22603697e62f47f14e525d9ee
blackslender <hoangvan10it@gmail.com>
2019-05-08 13:36:45 +0700

    Photo filters included

--- app/src/main/java/com/sbyb/PhotoEditorActivity.java
+++ app/src/main/java/com/sbyb/PhotoEditorActivity.java
@@ -1,16 +1,31 @@
 package com.sbyb;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
+
+import android.view.View;
+import android.widget.Button;
+
+import ja.burhanrashid52.photoeditor.PhotoEditor;
+import ja.burhanrashid52.photoeditor.PhotoEditorView;
+import ja.burhanrashid52.photoeditor.PhotoFilter;
 
 public class PhotoEditorActivity extends AppCompatActivity {
-    private TextView mTextMessage;
+
+    private Bitmap bitmap = null;
+    private Bitmap bitmap2 = null;
+    private Button button;
+    PhotoEditorView photoEditorView;
+    PhotoEditor photoEditor;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,8 +33,8 @@ public class PhotoEditorActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 //            switch (item.getItemId()) {
-//                case R.id.navigation_home:
-//                    mTextMessage.setText(R.string.title_home);
+//                case R.id.edit_effects:
+//                    mTextMessage.setText("Hello");
 //                    return true;
 //                case R.id.navigation_dashboard:
 //                    mTextMessage.setText(R.string.title_dashboard);
@@ -41,13 +56,43 @@ public class PhotoEditorActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_editor);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Load elements into variable
+        photoEditorView = findViewById(R.id.photoEditorView);
+
+        //Use custom font using latest support library
+        Typeface mTextRobotoTf = ResourcesCompat.getFont(this, R.font.roboto_medium);
+
+    //loading font from assest
+        Typeface mEmojiTypeFace = Typeface.createFromAsset(getAssets(), "emojione-android.ttf");
+
+        photoEditor = new PhotoEditor.Builder(this, photoEditorView)
+                .setPinchTextScalable(true)
+                .setDefaultTextTypeface(mTextRobotoTf)
+                .setDefaultEmojiTypeface(mEmojiTypeFace)
+                .build();
+
+
+        button = findViewById(R.id.button);
+        // Get dummy image into ImageView
+        bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.pic);
+
+
+        // Button
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                photoEditor.setFilterEffect(PhotoFilter.AUTO_FIX);
+            }
+        });
+
+
     }
 
     @Override
